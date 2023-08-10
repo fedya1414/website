2bf68aeded50d2fea6cf0b126a2e4f58237e7582
fedya1414 <136635231+fedya1414@users.noreply.github.com>
2023-08-10 18:16:30 +0300

    Add files via upload

---
+++ advertisements/app_advertisements/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from .models import Advertisement
+
+def index(request):
+    advertisements = Advertisement.objects.all()
+    context = {'advertisements': advertisements}
+    return render(request, 'index.html', context)
+
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
+
+def advertisement_post(request):
+    return render(request, 'advertisement-post.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def profile(request):
+    return render(request, 'profile.html')
+
+# Create your views here.
+
+
